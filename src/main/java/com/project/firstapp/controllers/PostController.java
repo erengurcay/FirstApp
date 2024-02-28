657f39e698bf3fbf278b8fd62619e05429155d2f
Eren Gurcay <geren.gurcay@istanbulticaret.edu.tr>
2024-02-28 12:45:36 +0300

    entities upgrade

---
+++ src/main/java/com/project/firstapp/controllers/PostController.java
@@ -0,0 +1,40 @@
+package com.project.firstapp.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
